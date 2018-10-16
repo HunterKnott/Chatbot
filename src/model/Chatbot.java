5a9c7f85ea2e31c101d42a35800c12b4b52f3d62
HunterKnott <hunterknott00@gmail.com>
2018-10-16 13:19:21 -0600

    Initial commit

--- src/model/Chatbot.java
+++ src/model/Chatbot.java
@@ -1,17 +1,33 @@
+//Diagram on umlet
 package model;
-
 import java.util.ArrayList;
-
 public class Chatbot
 {
-	private ArrayList<String> responseList = new ArrayList<>();
+	private ArrayList<String> responseList;
+	private ArrayList<String> spookyList;
 	private String content;
 	private String currentUser;
 	private String joke;
 	//____________________________________________
 	public Chatbot()
 	{
+		this.joke = "Why was Timmy sad?";
+		this.content = new String("Practical real talk from a friendly computer");
+		this.currentUser = new String("Very handsome man!");
 		
+		this.responseList = new ArrayList<String>();
+		this.spookyList = new ArrayList<String>();
+		
+		buildTheLists();
+	}
+	//------------------------
+	private void buildTheLists()
+	{
+		responseList.add("Good evening!");
+		responseList.add("Farewell...");
+		responseList.add("What do you call yourself?");
+		responseList.add("Tee Hee");
+		responseList.add("Delicious");
 	}
 	//------------------------
 	public Chatbot(String content)
@@ -19,6 +35,10 @@ public class Chatbot
 		
 	}
 	//______________________________________________
+	public ArrayList<String> getResponseList()
+	{
+		return responseList;
+	}
 	//---------------------------
 	public String getContent()
 	{
@@ -35,6 +55,10 @@ public class Chatbot
 		return joke;
 	}
 	//_______________________________________
+	public void setResponseList(ArrayList<String> responseList)
+	{
+		this.responseList = responseList;
+	}
 	//--------------------------------
 	public void setContent(String content)
 	{
