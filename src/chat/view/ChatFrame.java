535468e4ff31a990036ad955849c7b6e3e2a757c
HunterKnott <hunterknott00@gmail.com>
2018-12-03 13:28:25 -0700

    Made ChatFrame class

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -3,8 +3,28 @@
 package chat.view;
 
 import javax.swing.JFrame;
-
-public class ChatFrame
+import chat.controller.ChatController;
+//-------------------------------------------------------------
+public class ChatFrame extends JFrame
 {
-
+	private ChatController appController;
+	private ChatPanel appPanel;
+	//_____________________________________________________________
+	public ChatFrame(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		this.appPanel = new ChatPanel(appController);
+		//-------------------------------------------------------------
+		setupFrame();
+	}
+	//-------------------------------------------------------------
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setTitle("Chatbot");
+		this.setSize(1000, 500);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
