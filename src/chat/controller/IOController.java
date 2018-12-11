cefc34e9f80a72708b92e89326719809de59e5df
HunterKnott <hunterknott00@gmail.com>
2018-12-11 12:33:01 -0700

    Added scanner, file, and printWriter, with methods

--- src/chat/controller/IOController.java
+++ src/chat/controller/IOController.java
@@ -10,6 +10,30 @@ public class IOController
 {
 	public static void saveText(ChatController app, String path, String textToSave)
 	{
+		try
+		{
+			String filename = path;
+			Calendar date = Calendar.getInstance();
+			filename += "/" + date.get(Calendar.MONTH) + " " + date.get(Calendar.DAY_OF_MONTH);
+			filename += " chatbot save.txt"; 
+			
+			File saveFile = new File(filename);
+			Scanner textScanner = new Scanner(textToSave);
+			PrintWriter saveText = new PrintWriter(saveFile);
+			
+			while(textScanner.hasNext())
+			{
+				String currentLine = textScanner.nextLine();
+				saveText.println(currentLine);
+			}
+			
+			textScanner.close();
+			saveText.close();
+		}
 		
+		catch(IOException error)
+		{
+			
+		}
 	}
 }
